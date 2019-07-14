888f023959c5dfd0fa064793bf5312bd177dd3ea
DavidNorthup <david.northup97@gmail.com>
2019-07-13 20:15:45 -0700

    Added selection of device for output testing

--- application/Driver.cpp
+++ application/Driver.cpp
@@ -13,11 +13,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     bool done = false;
-    communication::SerialManager sm2(argv[1]);
-    communication::SerialManager sm(argv[2]);
+    communication::SerialManager sm(argv[1]);
+    communication::SerialManager sm2(argv[2]);
     std::string s;
     while (!done) {
-        std::cout << "Enter a binary number (up to 8 bits), or q to quit: ";
+        std::cout << "Enter a binary number (up to 5 bits), or q to quit: ";
         std::cin >> s;
         // Convert input to an int
         uint16_t message = 0;
@@ -30,7 +30,6 @@ int main(int argc, char *argv[]) {
                 if (s[i] == '0') { on = 0; }
                 message = (message << 1) + on;
             } 
-
             // Quit if the message contains q.
             if (s[i] == 'q') {
                 message = 0;
@@ -39,10 +38,21 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        std::cout << "Sending: " << message << std::endl;
-        // Send message and retrieve output
-        uint16_t c = sm.sendMessage(message);
-        std::cout << "Recieved: " << c << std::endl << std::endl;
+        
+        communication::SerialManager sm_temp = sm;
+        std::string selection;
+        if (!done) {
+            std::cout << "Which serial port would you like to output to? [1:" << argv[1] << "] or [2:" <<  argv[2] << "] - ";
+            std::cin >> selection;
+            std::cout << "Sending: " << message << std::endl;
+            if (selection.compare("1") != 0) {sm_temp = sm2;}
+            // Send message and retrieve output
+            uint16_t c = sm_temp.sendMessage(message);
+            std::cout << "Recieved: " << c << std::endl << std::endl;
+        } else {
+            uint16_t c = sm.sendMessage(message);
+            c = sm2.sendMessage(message);
+        }
     }
 
     std::cout << "Exiting" << std::endl;
